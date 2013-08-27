ab375bfc24e9aee77f1a098fe82d21e50b72d648
Seth VanHeulen <svanheulen@gmail.com>
2013-08-27 18:14:19 -0400

    set referer based on path and don't create output file until the request works

--- download.py
+++ download.py
@@ -20,14 +20,19 @@ import urllib2
 
 parser = argparse.ArgumentParser(description='downloads a page or file from the MHP3rd event quest website (hint: the index page is DL_TOP.PHP and you can find the other pages/files from there)')
 parser.add_argument('remotepath', help='remote file to download')
-parser.add_argument('output', type=argparse.FileType('wb'), help='local output file')
+parser.add_argument('output', help='local output file')
 args = parser.parse_args()
 
 uri = 'http://crusader.capcom.co.jp/psp/MHP3rd/'
-headers = {'User-Agent': 'Capcom Portable Browser v1.4 for MonsterHunterPortable3rd',
-           'Referer': uri + 'DL_MENU.PHP'}
+headers = {'User-Agent': 'Capcom Portable Browser v1.4 for MonsterHunterPortable3rd'}
+
+if args.remotepath.startswith('QUEST/'):
+    headers['Referer'] = uri + 'DL_MENU.PHP'
+elif args.remotepath.startswith('OTOMO/'):
+    headers['Referer'] = uri + 'DL_OTOMO.PHP'
+elif args.remotepath.startswith('BONUS/'):
+    headers['Referer'] = uri + 'DL_BONUS.PHP'
 
 request = urllib2.Request(uri + args.remotepath, '', headers)
 response = urllib2.urlopen(request)
-args.output.write(response.read())
-args.output.close()
+open(args.output, 'wb').write(response.read())
