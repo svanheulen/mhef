d2f1c304187ee8bb40d18872521814aa6bd26d08
Seth VanHeulen <svanheulen@gmail.com>
2013-10-20 00:58:15 -0400

    added license notice to python module

--- mhef/mhp3rd.py
+++ mhef/mhp3rd.py
@@ -1,3 +1,18 @@
+# Copyright 2013 Seth VanHeulen
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 DATA_EXCEPTIONS = (17, 18, 19, 20, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92)
 
 
