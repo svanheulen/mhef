bbce1b6281f7a719b495be8b6e5a3f25f6dbdfa8
Seth VanHeulen <svanheulen@gmail.com>
2015-06-24 13:40:54 -0400

    added savedata support for mh4u

--- mhef/n3ds.py
+++ mhef/n3ds.py
@@ -15,6 +15,7 @@
 
 import array
 import hashlib
+import random
 
 from Crypto.Cipher import Blowfish
 
@@ -28,6 +29,58 @@ MH4_EU = 5
 MH4G_JP = 6
 MH4G_NA = 7
 MH4G_EU = 8
+
+
+class SavedataCipher:
+    _mh4g_na_key = b'blowfish key iorajegqmrna4itjeangmb agmwgtobjteowhv9mope'
+
+    def __init__(self, game):
+        if game == MH4G_NA:
+            self._cipher = Blowfish.new(self._mh4g_na_key)
+        else:
+            raise ValueError('Ivalid game selected.')
+
+    def _xor(self, buff, key):
+        buff = array.array('H', buff)
+        for i in range(len(buff)):
+            if key == 0:
+                key = 1
+            key = key * 0xb0 % 0xff53
+            buff[i] ^= key
+        return buff.tobytes()
+
+    def encrypt(self, buff):
+        csum = sum(buff) & 0xffffffff
+        buff = array.array('I', buff)
+        buff.insert(0, csum)
+        seed = random.getrandbits(16)
+        buff = array.array('I', self._xor(buff.tobytes(), seed))
+        buff.insert(0, (seed << 16) + 0x10)
+        buff.byteswap()
+        buff = array.array('I', self._cipher.encrypt(buff.tobytes()))
+        buff.byteswap()
+        return buff.tobytes()
+
+    def decrypt(self, buff):
+        buff = array.array('I', buff)
+        buff.byteswap()
+        buff = array.array('I', self._cipher.decrypt(buff.tobytes()))
+        buff.byteswap()
+        seed = buff.pop(0) >> 16
+        buff = array.array('I', self._xor(buff.tobytes(), seed))
+        csum = buff.pop(0)
+        buff = buff.tobytes()
+        if csum != (sum(buff) & 0xffffffff):
+            raise ValueError('Invalid checksum in header.')
+        return buff
+
+    def encrypt_file(self, savedata_file, out_file):
+        with open(savedata_file, 'rb') as dlc, open(out_file, 'wb') as out:
+            out.write(self.encrypt(dlc.read()))
+
+    def decrypt_file(self, savedata_file, out_file):
+        with open(savedata_file, 'rb') as dlc, open(out_file, 'wb') as out:
+            out.write(self.decrypt(dlc.read()))
 
 
 class DLCCipher:
---
+++ savedata_n3ds.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+# Copyright 2015 Seth VanHeulen
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import argparse
+
+import mhef.n3ds
+
+
+parser = argparse.ArgumentParser(description='Encrypts or decrypts a savedata file from Monster Hunter 4 Ultimate')
+parser.add_argument('mode', choices=['e', 'd'], help='(e)ncrypt or (d)ecrypt')
+parser.add_argument('inputfile', help='savedata input file')
+parser.add_argument('outputfile', help='output file')
+args = parser.parse_args()
+
+sc = mhef.n3ds.SavedataCipher(mhef.n3ds.MH4G_NA)
+
+if args.mode == 'e':
+    sc.encrypt_file(args.inputfile, args.outputfile)
+else:
+    sc.decrypt_file(args.inputfile, args.outputfile)
+
