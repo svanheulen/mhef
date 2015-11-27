b31d1e42c182aea6f5555677663f8b746eea26cc
Seth VanHeulen <svanheulen@gmail.com>
2015-11-27 16:33:21 -0500

    added MHX download support

--- tools/download_n3ds.py
+++ tools/download_n3ds.py
@@ -23,21 +23,26 @@ except ImportError:
 
 
 parser = argparse.ArgumentParser(description='Downloads a file from the Monster Hunter 4 Ultimate DLC website')
+parser.add_argument('game', choices=('4G', 'X'), help='game version')
 parser.add_argument('region', choices=('JPN', 'USA', 'EUR', 'KOR', 'TWN'), help='game region')
 parser.add_argument('remotefile', help='remote file to download')
 parser.add_argument('outputfile', help='output file')
 args = parser.parse_args()
 
 uri = 'http://goshawk.capcom.co.jp/3ds/mh4g_nihon/{}'.format(args.remotefile)
-if args.region == 'USA':
-    uri = 'http://goshawk.capcom.co.jp/3ds/mh4g_us_/{}'.format(args.remotefile)
-elif args.region == 'EUR':
-    uri = 'http://goshawk.capcom.co.jp/3ds/mh4g_eu_/{}'.format(args.remotefile)
-elif args.region == 'KOR':
-    uri = 'http://goshawk.capcom.co.jp/3ds/mh4g_kr_/{}'.format(args.remotefile)
-elif args.region == 'TWN':
-    uri = 'http://goshawk4g.capcom.co.jp/redgiant/dl/pro_tw/{}'.format(args.remotefile)
 headers = {'User-Agent': 'Capcom Browser Services for MonsterHunter_4G'}
+if args.game == '4G':
+    if args.region == 'USA':
+        uri = 'http://goshawk.capcom.co.jp/3ds/mh4g_us_/{}'.format(args.remotefile)
+    elif args.region == 'EUR':
+        uri = 'http://goshawk.capcom.co.jp/3ds/mh4g_eu_/{}'.format(args.remotefile)
+    elif args.region == 'KOR':
+        uri = 'http://goshawk.capcom.co.jp/3ds/mh4g_kr_/{}'.format(args.remotefile)
+    elif args.region == 'TWN':
+        uri = 'http://goshawk4g.capcom.co.jp/redgiant/dl/pro_tw/{}'.format(args.remotefile)
+elif args.game == 'X':
+    uri = 'http://spector.capcom.co.jp/3ds/mhx_jp/{}'.format(args.remotefile)
+    headers = {'User-Agent': 'Capcom Browser Services for MonsterHunter_X'}
 
 request = Request(uri, headers=headers)
 response = urlopen(request)
