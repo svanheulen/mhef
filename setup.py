56f071384f087e050ecde44644675e19dd8f6701
Seth VanHeulen <svanheulen@gmail.com>
2016-01-03 10:39:37 -0500

    found new MHX encryption key

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ except ImportError:
 
 setup(
     name='mhef',
-    version='1.1.0',
+    version='1.1.1',
     description='Monster Hunter Encryption Functions',
     url='https://github.com/svanheulen/mhef',
     author='Seth VanHeulen',
