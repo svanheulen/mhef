a50593e1fc888f9629e7504d70838d1f4e766eaa
Seth VanHeulen <svanheulen@gmail.com>
2016-07-24 12:04:09 -0400

    made key script compatible with MHGen

--- tools/get_mhx_key.py
+++ tools/get_mhx_key.py
@@ -28,6 +28,9 @@ class Application:
         self._payload_written = False
         self._process_requested = False
         self._process = None
+        self._id = None
+        self._object1 = 0
+        self._object2 = 0
         self._key = None
         self._host = host
 
@@ -42,18 +45,20 @@ class Application:
         while len(packet_data) < packet_header[20]:
             packet_data += self._socket.recv(packet_header[20] - len(packet_data))
         if packet_header[3] == 0:
-            if re.search(b'finishedfinished', packet_data):
-                self._payload_written = True
-                print('    proceed to download section')
-                return
-            m = re.search(b'pid: 0x([0-9a-f]{8}), pname: Festa_Ro', packet_data)
+            m = re.search(b'pid: 0x([0-9a-f]{8}), pname: Festa_Ro, tid: ([0-9a-f]{16}), ', packet_data)
             if m:
                 self._process = int(m.group(1), 16)
+                self._id = m.group(2)
                 print('    process id is {}'.format(self._process))
         elif packet_header[3] == 9:
-            self._key = packet_data.decode().strip('\x00')
-            if self._key == '':
-                self._key = None
+            if self._object1 == 0:
+                self._object1 = array.array('I', packet_data)[0]
+            elif self._object2 == 0:
+                self._object2 = array.array('I', packet_data)[0]
+            else:
+                self._key = packet_data.decode().strip('\x00')
+                if self._key == '':
+                    self._key = None
 
     def _send_packet(self, packet_type, command, args=[], data=b''):
         self._sequence += 1000
@@ -71,25 +76,34 @@ class Application:
         while True:
             packet = self._read_packet()
             if self._key is not None:
-                print('    key is {}'.format(self._key))
-                print('use the home button to close MHX without saving')
+                if self._id == b'0004000000155400':
+                    print('    MHX JPN key = {}'.format(self._key))
+                elif self._id == b'0004000000187000':
+                    print('    MHGen USA key = {}'.format(self._key))
+                elif self._id == b'0004000000185b00':
+                    print('    MHGen EUR key = {}'.format(self._key))
                 return
             elif not self._process_requested:
                 print('getting process ...')
                 self._send_packet(0, 5)
                 self._send_packet(0, 0)
                 self._process_requested = True
-            elif self._process is not None and not self._payload_sent:
-                print('writing hook ...')
-                #self._send_packet(0, 10, [self._process, 0xc01e9c, 36], b'\xf0G-\xe9\x00@\xa0\xe1\xeb\xab\xf6\xeb\x08 \x94\xe5\x04\x10\x94\xe5\x04\x00\x9f\xe5p\xeb\xdb\xeb\xf0\x87\xbd\xe8\x00\xce\xe5\x00')
-                self._send_packet(0, 10, [self._process, 0xc01ffc, 32], b'\xf0G-\xe9\x00@\xa0\xe1\xd7\xad\xf6\xeb\x08 \x94\xe5\x04\x10\x94\xe5\xe5\x08\xa0\xe3\x1a\xeb\xdb\xeb\xf0\x87\xbd\xe8')
-                #self._send_packet(0, 10, [self._process, 0x2488e0, 4], b'm\xe5&\xeb')
-                self._send_packet(0, 10, [self._process, 0x2488e8, 4], b'\xc3\xe5&\xeb')
-                self._send_packet(0, 0)
-                self._payload_sent = True
-            elif self._payload_written and self._sequence % 5000 == 0:
-                print('checking for key ...')
-                self._send_packet(0, 9, [self._process, 0xe50000, 60])
+            elif self._process and self._sequence % 5000 == 0:
+                if self._object1 == 0:
+                    print('checking for object 1 ...')
+                    if self._id == b'0004000000155400':
+                        self._send_packet(0, 9, [self._process, 0xdba8f4, 4])
+                    else:
+                        self._send_packet(0, 9, [self._process, 0xda28f4, 4])
+                elif self._object2 == 0:
+                    print('checking for object 2 ...')
+                    self._send_packet(0, 9, [self._process, self._object1 + 0x34, 4])
+                else:
+                    print('checking for key ...')
+                    if self._id == b'0004000000155400':
+                        self._send_packet(0, 9, [self._process, self._object2 + 0x2a1, 128])
+                    else:
+                        self._send_packet(0, 9, [self._process, self._object2 + 0x5a0, 128])
                 self._send_packet(0, 0)
             else:
                 self._send_packet(0, 0)
@@ -101,8 +115,8 @@ args = parser.parse_args()
 
 app = Application(args.host)
 try:
-    raw_input("enabled the NTR debugger and go to the MHX main title menu, then press enter")
+    raw_input("enabled the NTR debugger and go to the MHX/Gen download section, then press enter")
 except:
-    input("enabled the NTR debugger and go to the MHX main title menu, then press enter")
+    input("enabled the NTR debugger and go to the MHX/Gen download section, then press enter")
 app.run()
 
